2002830bfc2be3091f83550bccbfea788e9ccabf
burhanghani <gburhanghani01@gmail.com>
2024-10-19 18:38:00 +0500

    modall test

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,26 @@
-import { Text, View } from "react-native";
-import { Button } from "react-native-paper";
+import React from "react";
+import { Button, Text, View } from "react-native";
+import { Modal, PaperProvider, Portal, Provider } from "react-native-paper";
 
 export default function Index() {
+
+  const [visible, setVisible] = React.useState(false);
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+
+  const containerStyle = { backgroundColor: 'white', padding: 20, margin: 20 };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>My name is Abubaker Imran.</Text>
-      <Text>My name is RJ.</Text>
-      <View>
-        <Button mode="outlined">sahvdahs</Button>
+    <Provider>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Button title="Open Modal" onPress={showModal} />
+        <Portal>
+          <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+            <Text>You have opened the modal</Text>
+          </Modal>
+        </Portal>
       </View>
-    </View>
+    </Provider>
   );
-}
+};
